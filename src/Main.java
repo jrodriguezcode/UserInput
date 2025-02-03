0676b2c0fef521b787fb9b6149e17e00fea3629d
jrodriguezcode <jr698751@gmail.com>
2025-02-03 13:47:45 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("What is your name?");
+		String name = in.nextLine();
+		
+		System.out.println("How old are you?");
+		int age = in.nextInt();
+		in.nextLine(); // This is needed to clear out the Scanner so that the nextLine() method is actually usable
+		
+		System.out.println("What is your favorite food?");
+		String food = in.nextLine();
+		
+		System.out.println("Hello " + name);
+		System.out.println("You are " + age + " years old");
+		System.out.println("You like " + food);
+	}
+
+}
